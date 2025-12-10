c7616f56f47089e641f7987de1f35301b22d11fb
AngeloSalzillo <a.salzillo.pro@gmail.com>
2025-12-10 15:23:57 +0100

    Create xgboost_bayes_search.py

---
+++ main/xgboost_bayes_search.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from src.train_xgboost import load_data, train_xgboost
+
+BASE_DIR = "/kaggle/input/house-prices-advanced-regression-techniques"
+SAVE_DIR = "results"
+
+X_train, y_train, X_test, ids = load_data(BASE_DIR)
+
+preds = train_xgboost(X_train, y_train, X_test, save_dir=SAVE_DIR)
+
+submission = pd.DataFrame({
+    "Id": ids,
+    "SalePrice": preds
+})
+
+submission.to_csv(f"{SAVE_DIR}/submission.csv", index=False)
+print("Submission saved!")
